8b7506d65a68dcbc9ce5552d7ce0ebfa20e674c5
zxq5 <zxq5@zxq5.dev>
2025-05-16 17:56:13 +0100

    done something

--- main.go
+++ main.go
@@ -3,5 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("hello world");
+	fmt.Println("hello world")
+
+	game := Game{
+		profit:    0,
+		pollution: 0}
+
+	fmt.Printf("Game: %d", game.profit)
+}
+
+type Game struct {
+	profit    int
+	pollution int
 }
